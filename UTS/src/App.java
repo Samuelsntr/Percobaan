3e56cdb39020e174fd8ca04db0c1384548cddad1
Samuelsntr <dodisamuel20@gmail.com>
2021-11-06 00:20:45 +0800

    2

--- UTS/src/App.java
+++ UTS/src/App.java
@@ -1,19 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // a. 1 instance yang hanya anggota (isikan nama dan noAnggota)
-        Anggota dewak = new Anggota("Dewak", "01");
-        dewak.display();
+        Anggota orang1 = new Anggota("Dewak", "01");
+        orang1.display();
+
         System.out.println("#################################");
         // b. 1 instance ketua pengurus (isikan nama, noAnggota dan periode kepengurusan)
-        Ketua garry = new Ketua("Garry", "02", "2019 / 2020");
-        garry.display();
+        Ketua orang2 = new Ketua("Garry", "02", "2019 / 2020");
+        orang2.display();
         
         System.out.println("#################################");
         // c. 1 instance ketua panitia (isikan nama, noAnggota dan periode kepengurusan)
-        KetuaPanitia roger = new KetuaPanitia("Roger", "03", "2020 / 2021");
-        roger.display();
+        KetuaPanitia orang3 = new KetuaPanitia("Roger", "03", "2020 / 2021");
+        orang3.display();
 
-        System.out.println("coba dulu");
-        System.out.println("satu lagi");
     }
 }
